4a459725d4697ee1db9c670f878f4b092072a42c
jordan <jordanaw0117@gmail.com>
2022-02-07 07:41:31 +0700

    third

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -14,6 +14,8 @@
         </li>
         <li class="nav-item">
           <a class="nav-link {{ ( $title === 'Gallery') ? 'active': '' }}" href="/gallery">Gallery</a>
+        <li class="nav-item">
+          <a class="nav-link {{ ( $title === 'Contacts') ? 'active': '' }}" href="/contacts">Contact Us</a>
         </li>
       </ul>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,3 +38,5 @@ Route::get('/gallery', function () {
         "title" => "Gallery"
     ]);
 });
+
+Route::resource('/contacts', ContactController::class);
